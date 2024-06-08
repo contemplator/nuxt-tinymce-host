b756a3c9a9ced4610127e2d06194a21208f6fd73
Leo Lin <contemplation8213@gmail.com>
2024-06-08 16:02:15 +0800

    fix: remove tinymce does not exist on type window warning

--- plugins/tinymce.client.ts
+++ plugins/tinymce.client.ts
@@ -1,13 +1,19 @@
-import { defineNuxtPlugin } from '#app';
+import { defineNuxtPlugin } from "#app";
+declare global {
+  interface Window {
+    tinymce: any;
+  }
+}
+
 // import tinymce from 'tinymce';
 
 export default defineNuxtPlugin(() => {
   if (process.client) {
-    const script = document.createElement('script');
-    script.src = '/tinymce/tinymce.min.js';
+    const script = document.createElement("script");
+    script.src = "/tinymce/tinymce.min.js";
     script.async = true;
     script.onload = () => {
-      console.log('TinyMCE loaded', tinymce);
+      console.log("TinyMCE loaded", window.tinymce);
     };
     document.head.appendChild(script);
   }
