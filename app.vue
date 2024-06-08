275675ac4ad6b71c9c5d6e0516368759ac38515f
Leo Lin <contemplation8213@gmail.com>
2024-06-08 16:02:21 +0800

    fix: format

--- app.vue
+++ app.vue
@@ -1,32 +1,36 @@
 <script lang="ts" setup>
 onMounted(() => {
   const initTinyMCE = () => {
-    if (typeof window.tinymce !== 'undefined') {
+    if (typeof window.tinymce !== "undefined") {
       window.tinymce.init({
-        selector: '#editor',
+        selector: "#editor",
         height: 500,
         menubar: false,
-        plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount',
-        toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
+        plugins:
+          "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount",
+        toolbar:
+          "undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
       });
     } else {
-      console.error('TinyMCE is not defined on window object');
+      console.error("TinyMCE is not defined on window object");
     }
   };
 
   // 檢查 TinyMCE 是否已加載
-  if (typeof window.tinymce !== 'undefined') {
+  if (typeof window.tinymce !== "undefined") {
     initTinyMCE();
   } else {
     // 等待 TinyMCE 加載完成
-    window.addEventListener('load', initTinyMCE);
+    window.addEventListener("load", initTinyMCE);
   }
 });
 
 const setContent = () => {
-  console.log('test');
+  console.log("test");
   // window.tinymce.activeEditor.setContent('<a href="https://www.google.com" target="_blank">Hello, World!</a>');
-  window.tinymce.activeEditor.setContent('<iframe width="560" height="315" src="https://www.youtube.com/embed/oPZFuijjXrc?si=UM3P6_p3PpBvRofs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>');
+  window.tinymce.activeEditor.setContent(
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/oPZFuijjXrc?si=UM3P6_p3PpBvRofs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+  );
 };
 
 const getContent = () => {
@@ -34,11 +38,13 @@ const getContent = () => {
 };
 
 const insertContent = () => {
-  window.tinymce.activeEditor.selection.setContent('Inserted Text');
+  window.tinymce.activeEditor.selection.setContent("Inserted Text");
 };
 
 const copyContent = () => {
-  navigator.clipboard.writeText('<a href="https://www.google.com.tw">Google</a>');
+  navigator.clipboard.writeText(
+    '<a href="https://www.google.com.tw">Google</a>'
+  );
 };
 </script>
 
